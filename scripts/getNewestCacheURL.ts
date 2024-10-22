6fac37d2f4f29159a3ca1f3936511a7559b6268f
Wouter van der Plas <2423856+wvanderp@users.noreply.github.com>
2024-10-22 21:44:36 +0200

    download the cache

--- scripts/getNewestCacheURL.ts
+++ scripts/getNewestCacheURL.ts
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import fs from 'fs';
+import yauzl from 'yauzl';
 
 const url = 'https://archive.openrs2.org/caches.json';
 
@@ -28,16 +30,59 @@ export interface OsrsCache {
 
 (async () => {
 
-    const response = await fetch(url);
+    console.log('Fetching cache URL...');
+    const response = await axios.get(url);
 
-    if (!response.ok) {
+    if (response.status !== 200) {
         throw new Error(`Failed to fetch URL: ${response.statusText}`);
     }
 
-    const json = await response.json() as OsrsCache[];
+    const json = await response.data as OsrsCache[];
 
     const newest = json
-        .filter((cache) => cache.game === 'oldschool');
+        .filter((cache) => cache.game === 'oldschool')
+        .filter((cache) => cache.environment === 'live')
+        .sort((a, b) => { 
+            // date descending
+            return new Date(b.timestamp!).getTime() - new Date(a.timestamp!).getTime();
+        })[0];
 
-    fs.writeFileSync('newestCacheURL.json', JSON.stringify(newest, null, 2));
+    
+    // get the newest cache and unzip it and stream it to file
+    console.log(`Downloading cache ${newest.id}...`);
+    console.log(`Cache URL: https://archive.openrs2.org/caches/runescape/${newest.id}`);
+    const cacheURL = `https://archive.openrs2.org/caches/runescape/${newest.id}/disk.zip`;
+
+    const cache = await axios.get(cacheURL, {
+        responseType: 'arraybuffer'
+    })
+
+    fs.writeFileSync('cache.zip', cache.data);
+
+    console.log('Unzipping cache...');
+
+    if (fs.existsSync('cache')) {
+        fs.rmdirSync('cache', {recursive: true});
+    }
+
+    fs.mkdirSync('cache');
+
+    yauzl.open('cache.zip', {lazyEntries: true}, (err, zipfile) => {
+        if (err) throw err;
+
+        zipfile.readEntry();
+        zipfile.on('entry', (entry) => {
+            if (/\/$/.test(entry.fileName)) {
+                zipfile.readEntry();
+            } else {
+                zipfile.openReadStream(entry, (err, readStream) => {
+                    if (err) throw err;
+                    readStream.on('end', () => {
+                        zipfile.readEntry();
+                    });
+                    readStream.pipe(fs.createWriteStream(entry.fileName));
+                });
+            }
+        });
+    });
 })();
