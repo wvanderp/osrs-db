d5879f62fe6a22c471ef91d2596a8617e2d13d02
Wouter van der Plas <wvanderplas1@lely.com>
2024-11-11 01:20:46 +0100

    will lint the Items.json

--- scripts/lintData.ts
+++ scripts/lintData.ts
@@ -1,5 +1,7 @@
+import items from './linter/items.linter.ts';
 import slotStats from './linter/slotStats.linter.ts';
 import titleToId from './linter/titleToID.linter.ts';
 
 titleToId();
 slotStats();
+items();
---
+++ scripts/linter/items.linter.ts
@@ -0,0 +1,26 @@
+import Ajv from "ajv";
+import fs from "fs";
+import path from "path";
+
+// load the file
+const filePath = path.join(__dirname, "../../data/items.json");
+const file = JSON.parse(fs.readFileSync(filePath, "utf8")) as { [key: string]: number | string | null }[];
+
+const schemaPath = path.join(__dirname, "../../schemas/items.schema.json");
+const schema = JSON.parse(fs.readFileSync(schemaPath, "utf8"));
+
+export default function items() {
+    console.log("Linting items.json");
+    // #region schema
+    const ajv = new Ajv();
+    const validate = ajv.compile(schema);
+
+    const valid = validate(file);
+
+    if (!valid) {
+        console.log(validate.errors);
+    }
+    // #endregion
+
+    console.log("items.json is valid");
+}
